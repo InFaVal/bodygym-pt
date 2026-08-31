afed20076a7b9d24cbd920af50dbc3bdde1adf73
InFaVal <ignacio.famoso@gmail.com>
2026-08-31 20:08:56 +0200

    Use network-first PWA cache during active development

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-const CACHE_NAME="bodygym-pt-v6";const APP_ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./data/torso-a.json","./data/pierna-a.json","./data/torso-b.json","./data/pierna-b.json","./assets/icon.svg","./assets/dominadas.svg","./assets/press_inclinado.svg","./assets/poleas.svg","./assets/contractor.svg","./assets/pierna_maquinas.svg","./assets/hiperextensiones.svg","./assets/zona_azul.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_ASSETS)).catch(()=>{})));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).catch(()=>caches.match("./index.html")))));
+const CACHE_NAME="bodygym-pt-v7";
+const APP_ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./data/torso-a.json","./data/pierna-a.json","./data/torso-b.json","./data/pierna-b.json","./assets/icon.svg","./assets/dominadas.svg","./assets/press_inclinado.svg","./assets/poleas.svg","./assets/contractor.svg","./assets/pierna_maquinas.svg","./assets/hiperextensiones.svg","./assets/zona_azul.svg"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_ASSETS)).catch(()=>{}));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(
+    fetch(event.request,{cache:"no-store"}).then(response=>{
+      if(response && response.ok && event.request.url.startsWith(self.location.origin)){
+        const copy=response.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      }
+      return response;
+    }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html")))
+  );
+});
